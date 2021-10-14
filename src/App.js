f4a7e4e6e8679604603eeb9712d528b670f23365
waelkhrdir <37935096+waelkhrdir@users.noreply.github.com>
2021-10-14 16:08:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import taswira from './wl.jpg'
+import "./style.css"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+             <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">waelbk</h1>
+          <br />
+          <img src={taswira} />
+          <br />
+          <img src="wael.jpg"/>
+        </div>
+        <video width={320} height={240} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
